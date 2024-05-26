cfd7165dfd12dfd6456573737cc56a164584246a
kjh <100295423+readonlymemory1@users.noreply.github.com>
2024-05-26 15:16:48 +0900

    임시저장

--- RISA_lib/_.py
+++ RISA_lib/_.py
@@ -1,7 +1,7 @@
 import pandas as pd
 a = pd.read_csv("C:/Users/kjh05/OneDrive/문서/GitHub/RISA_lib/RISA_lib/q_a.csv")
 index = a[a["ai"]=="w"].index
-print(a[index])
+print(a[][index])
 # dataset = pd.DataFrame(
 #     a["ai"][]
 # )
